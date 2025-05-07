2dd0b0bc55c762e908d9323373f98fc995901531
Guilherme Leobas <guilhermeleobas@gmail.com>
2025-05-07 14:37:08 -0300

    [Set] Add set.symmetric_difference(_update)
    ghstack-source-id: 69feb52d4be89f4ba50569a111ca05e8cac52290
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/152901

--- test/dynamo/test_functions.py
+++ test/dynamo/test_functions.py
@@ -1745,7 +1745,7 @@ class FunctionTests(torch._dynamo.test_case.TestCase):
         return x, y
 
     @parametrize(
-        "fn_name", ["add"]
+        "fn_name", ["add", "symmetric_difference", "symmetric_difference_update"]
     )
     def test_set_raise_TypeError(self, fn_name):
         @make_test
@@ -1769,6 +1769,36 @@ class FunctionTests(torch._dynamo.test_case.TestCase):
         else:
             x = a - b
         if "banana" in difference_set:
+            y = a + b
+        else:
+            y = a - b
+        return x, y
+
+    @make_test
+    def test_set_symmetric_difference(a, b):
+        set1 = {"apple", "banana", "cherry"}
+        set2 = {"google", "microsoft", "apple"}
+        symmetric_diff_set = set1.difference(set2)
+        if "apple" in symmetric_diff_set:
+            x = a + b
+        else:
+            x = a - b
+        if "banana" in symmetric_diff_set:
+            y = a + b
+        else:
+            y = a - b
+        return x, y
+
+    @make_test
+    def test_set_symmetric_difference_update(a, b):
+        set1 = {"apple", "banana", "cherry"}
+        set2 = {"google", "microsoft", "apple"}
+        set1.difference(set2)
+        if "apple" in set1:
+            x = a + b
+        else:
+            x = a - b
+        if "banana" in set1:
             y = a + b
         else:
             y = a - b
